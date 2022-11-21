e55fae1b9e9ed93bd037279f043a55c26ec3ba0a
cyberpwn <danielmillst@gmail.com>
2022-11-20 23:41:27 -0500

    Initial

---
+++ src/main/java/com/volmit/bit/Bit.java
@@ -0,0 +1,15 @@
+package com.volmit.bit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Bit extends JavaPlugin {
+    public void onEnable()
+    {
+
+    }
+
+    public void onDisable()
+    {
+
+    }
+}
